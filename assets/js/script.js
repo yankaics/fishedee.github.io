8c0fece65e59152c6a250ec9d66b42e00f4d4ae6
fishedee <306766045@qq.com>
2016-03-15 13:41:05 +0800

    优化细节

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,21 +144,22 @@ function afterPjax() {
 
   // Lazy Loading Disqus
   // http://jsfiddle.net/dragoncrew/SHGwe/1/
-  var ds_loaded = false,
-      top = $('#disqus_thread').offset().top;
-      identifier = $('#post__title').data('identifier');
+  
+  var ds_loaded = false;
 
   function check() {
-    if ( !ds_loaded && container.scrollTop() + container.height() > top ) {
-      $.ajax({
-        type: 'GET',
-        url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
-        dataType: 'script',
-        cache: true
-      });
-      ds_loaded = true;
+    if( ds_loaded){
+      return;
     }
-  }check();
-  container.scroll(check);
-}afterPjax();
+    $.ajax({
+      type: 'GET',
+      url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
+      dataType: 'script',
+      cache: true
+    });
+    ds_loaded = true;
+  }
+  check();
+}
+afterPjax();
 
---
+++ index.markdown
@@ -0,0 +1,18 @@
+---
+layout: post
+title: 关于我
+---
+
+华中科技大学软件硕士毕业，混迹过不知名小公司 / 网易 / 阿里巴巴，目前在美团猫眼做后台开发。喜欢写代码，更喜欢做东西，自娱自乐。爱好 dota（天梯1100分的水平），皮艺手工（做了几个钱包）。近期最大的目标是把体重提高到 130。
+
+# 关于这个博客
+
+托管在 github，模板使用 [3-jekyll](https://github.com/P233/3-Jekyll)
+这里大部分是我平常总结的笔记，从 evernote 同步过来的，写了个[python 脚本](https://github.com/novoland/blog/blob/master/evernote-sync/main.py)干这事儿。大部分属于原创，如果有参考文档会尽量写在博客里，[这里](https://github.com/novoland/blog/blob/master/evernote-sync/settings.py)可以看到我所有公开的笔记。
+
+博客使用的工具：
+
+* [马克飞象](http://maxiang.info)
+* [3-jekyll](https://github.com/P233/3-Jekyll)
+* python
+* [Evernote](http://evernote.com)
